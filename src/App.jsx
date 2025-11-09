dbb4486dbe775fddc0f6c2974278403422fa9d67
obiwan24774-alt <obiwan24774@gmail.com>
2025-11-09 23:55:21 +0900

    スクロール機能削除

--- src/App.jsx
+++ src/App.jsx
@@ -20,27 +20,29 @@ function App() {
 
   return (
     <Router>
-      <div className="flex flex-col items-center min-h-screen pt-[15vh] ...">
-        <h1 className="text-blue-600 bg-white text-3xl font-bold mt-10">
-          トレーニング記録
-        </h1>
-        <Routes>
-          <Route path="/" element={<RecordForm onSave={handleSave} />} />
-          <Route
-            path="/records"
-            element={
-              <RecordListPage records={records} onDelete={handleDelete} />
-            }
-          />
-        </Routes>
+      <div className="relative h-screen overflow-hidden">
+        <div className="flex flex-col items-center pt-[15vh]">
+          <h1 className="text-blue-600 bg-white text-3xl font-bold mt-10">
+            トレーニング記録
+          </h1>
+          <Routes>
+            <Route path="/" element={<RecordForm onSave={handleSave} />} />
+            <Route
+              path="/records"
+              element={
+                <RecordListPage records={records} onDelete={handleDelete} />
+              }
+            />
+          </Routes>
 
-        <div className="mt-6 text-center">
-          <Link
-            to="/records"
-            className="mt-4 text-blue-500 hover:underline block text-center"
-          >
-            記録一覧を見る
-          </Link>
+          <div className="mt-6 text-center">
+            <Link
+              to="/records"
+              className="mt-4 text-blue-500 hover:underline block text-center"
+            >
+              記録一覧を見る
+            </Link>
+          </div>
         </div>
       </div>
     </Router>
